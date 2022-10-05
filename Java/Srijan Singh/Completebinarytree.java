2c0faa8f3be21735c5d75676ad57e4a21486cd2e
srijansingh <srijansingh82003@gmail.com>
2022-10-05 12:22:23 +0530

    Added code to check CBT

---
+++ Java/Srijan Singh/Completebinarytree.java
@@ -0,0 +1,56 @@
+// Checking if a binary tree is a complete binary tree in Java
+
+// Node creation
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int item) {
+        data = item;
+        left = right = null;
+    }
+}
+
+class BinaryTree {
+    Node root;
+
+    // Count the number of nodes
+    int countNumNodes(Node root) {
+        if (root == null)
+            return (0);
+        return (1 + countNumNodes(root.left) + countNumNodes(root.right));
+    }
+
+    // Check for complete binary tree
+    boolean checkComplete(Node root, int index, int numberNodes) {
+
+        // Check if the tree is empty
+        if (root == null)
+            return true;
+
+        if (index >= numberNodes)
+            return false;
+
+        return (checkComplete(root.left, 2 * index + 1, numberNodes)
+                && checkComplete(root.right, 2 * index + 2, numberNodes));
+    }
+
+    public static void main(String args[]) {
+        BinaryTree tree = new BinaryTree();
+
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.right = new Node(5);
+        tree.root.left.left = new Node(4);
+        tree.root.right.left = new Node(6);
+
+        int node_count = tree.countNumNodes(tree.root);
+        int index = 0;
+
+        if (tree.checkComplete(tree.root, index, node_count))
+            System.out.println("The tree is a complete binary tree");
+        else
+            System.out.println("The tree is not a complete binary tree");
+    }
+}
